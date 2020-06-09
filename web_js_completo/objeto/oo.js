c9894fb2c4eb270767d3bd86cfb8dd03bcf73c46
arthuralv <arthur.alves@ccc.ufcg.edu.br>
2020-06-09 18:39:09 -0300

    Começo das Aulas de Objetos

---
+++ web_js_completo/objeto/oo.js
@@ -0,0 +1,25 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+
+// Procedural 
+
+processamento(variavel1, variavel2, variavel3)
+
+// OO
+
+objeto = {
+    atributo1,
+    atributo2,
+
+    processamento()
+}
+
+objeto.processamento()
+
+/* Princípios Importantes:
+    1. abstração
+    2. encapstulamento
+    3. polimorfismo
+        3.1 herança (prototype)
+        3.2 composição
+
+*/
